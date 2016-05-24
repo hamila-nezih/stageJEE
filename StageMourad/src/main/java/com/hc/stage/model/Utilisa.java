5f4a96991da62b397d3f051277e228ae381274ad
hamila <hamila90.nezih@gmail.com>
2016-05-24 18:00:30 +0200

    spring security

---
+++ StageMourad/src/main/java/com/hc/stage/model/Utilisa.java
@@ -0,0 +1,26 @@
+package com.hc.stage.model;
+
+public class Utilisa {
+	private String jour;
+	private  String idClient;
+	private long idCreneau;
+	public String getJour() {
+		return jour;
+	}
+	public void setJour(String jour) {
+		this.jour = jour;
+	}
+	
+	public long getIdCreneau() {
+		return idCreneau;
+	}
+	public void setIdCreneau(long idCreneau) {
+		this.idCreneau = idCreneau;
+	}
+	public String getIdClient() {
+		return idClient;
+	}
+	public void setIdClient(String idClient) {
+		this.idClient = idClient;
+	}
+}
